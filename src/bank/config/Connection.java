14c540fb40175a8ec216c647156220a8debd2336
Rémi Campistron <remi.campistron@gmail.com>
2026-02-11 16:00:19 +0100

    Squelette de la classe connectionInstantiator

---
+++ src/bank/config/Connection.java
@@ -0,0 +1,5 @@
+package bank.config;
+
+public interface Connection {
+
+}
--- src/bank/config/ConnectionInstantiator.java
+++ src/bank/config/ConnectionInstantiator.java
@@ -2,5 +2,25 @@ package bank.config;
 
 /* Instantiate a new connection, uses singleton design pattern */
 public class ConnectionInstantiator {
+ static ConnectionInstantiator instance;
+ private Connection connection;
+ private String url;
+ private String user;
+ private String password;
+
+ private ConnectionInstantiator(){
+
+ }
+ public static ConnectionInstantiator getInstance(){
+
+ }
+
+ public static Connection getConnection(){
+
+ }
+
+ public static void closeConnection(){
+
+ }
 
 }
