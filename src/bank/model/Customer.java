8fb1bf913d337cf828460a1014a8bebe8b7a950b
Rémi Campistron <remi.campistron@gmail.com>
2026-02-12 16:16:04 +0100

    Création du constructeur + getters/setters de la classe Customer

--- src/bank/model/Customer.java
+++ src/bank/model/Customer.java
@@ -1,5 +1,61 @@
 package bank.model;
 
-public class Customer extends User {
+public class Customer {
+  int id;
+  String firstName;
+  String lastName;
+  String email;
+  String phone;
+
+  public Customer(int id, String firstName, String lastName, String email, String phone) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.phone = phone;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+
+
 
 }
