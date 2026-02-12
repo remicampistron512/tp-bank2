8c931e23c881ad170779123e959689619947ce35
Rémi Campistron <remi.campistron@gmail.com>
2026-02-12 15:48:30 +0100

    Implémentation de la connexion sql

--- src/bank/config/ConnectionInstantiator.java
+++ src/bank/config/ConnectionInstantiator.java
@@ -1,26 +1,51 @@
 package bank.config;
 
-/* Instantiate a new connection, uses singleton design pattern */
-public class ConnectionInstantiator {
- static ConnectionInstantiator instance;
- private Connection connection;
- private String url;
- private String user;
- private String password;
+import bank.dao.DaoException;
 
- private ConnectionInstantiator(){
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
- }
- public static ConnectionInstantiator getInstance(){
+public final class ConnectionInstantiator {
 
- }
+  private static final String PROPS_FILE = "db.properties";
+  private final Properties props;
 
- public static Connection getConnection(){
+  private static final ConnectionInstantiator INSTANCE = new ConnectionInstantiator();
 
- }
+  private ConnectionInstantiator() {
+    this.props = loadProps();
+  }
 
- public static void closeConnection(){
+  public static ConnectionInstantiator getInstance() {
+    return INSTANCE;
+  }
 
- }
+  private static Properties loadProps() {
+    Properties p = new Properties();
+    try (InputStream in = ConnectionInstantiator.class.getClassLoader().getResourceAsStream(PROPS_FILE)) {
+      if (in == null) {
+        throw new DaoException("Cannot find " + PROPS_FILE + " on classpath (put it in resources/).");
+      }
+      p.load(in);
+      return p;
+    } catch (IOException e) {
+      throw new DaoException("Cannot load " + PROPS_FILE + ".", e);
+    }
+  }
 
+  public Connection getConnection() {
+    try {
+      return DriverManager.getConnection(
+          props.getProperty("db.url"),
+          props.getProperty("db.user"),
+          props.getProperty("db.password")
+      );
+    } catch (SQLException e) {
+      throw new DaoException("Cannot connect to MySQL. Check db.properties.", e);
+    }
+  }
 }
