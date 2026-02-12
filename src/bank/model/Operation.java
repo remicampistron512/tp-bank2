1f8a796a2326bd05211d07aeedef7c629e318d0e
Rémi Campistron <remi.campistron@gmail.com>
2026-02-12 16:17:24 +0100

    Création du constructeur + getters/setters de la classe Operation

--- src/bank/model/Operation.java
+++ src/bank/model/Operation.java
@@ -1,5 +1,60 @@
 package bank.model;
 
-public class Operation {
+import java.math.BigDecimal;
+import java.security.Timestamp;
 
+public class Operation {
+  int id;
+  int accountId;
+  String type;
+  BigDecimal amount;
+  Timestamp operationDate;
+
+  public Operation(int id, int accountId, String type, BigDecimal amount, Timestamp operationDate) {
+    this.id = id;
+    this.accountId = accountId;
+    this.type = type;
+    this.amount = amount;
+    this.operationDate = operationDate;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getAccountId() {
+    return accountId;
+  }
+
+  public void setAccountId(int accountId) {
+    this.accountId = accountId;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public BigDecimal getAmount() {
+    return amount;
+  }
+
+  public void setAmount(BigDecimal amount) {
+    this.amount = amount;
+  }
+
+  public Timestamp getOperationDate() {
+    return operationDate;
+  }
+
+  public void setOperationDate(Timestamp operationDate) {
+    this.operationDate = operationDate;
+  }
 }
