f6e1bb9f4ab28972553d8d5797ff0dc00e9e0438
Rémi Campistron <remi.campistron@gmail.com>
2026-02-12 16:12:59 +0100

    Création de la classe CheckingAccount dans model

---
+++ src/bank/model/CheckingAccount.java
@@ -0,0 +1,10 @@
+package bank.model;
+
+import java.math.BigDecimal;
+
+public class CheckingAccount extends Account {
+
+  public CheckingAccount(int id, BigDecimal balance, String type) {
+    super(id, balance, type);
+  }
+}
