3dddbc82cfa08e1fb5924bff5173b9bac2ff1cc8
Rémi Campistron <remi.campistron@gmail.com>
2026-02-13 13:30:33 +0100

    Implémentation du service BankingService

--- src/bank/service/BankingService.java
+++ src/bank/service/BankingService.java
@@ -1,5 +1,71 @@
 package bank.service;
+import bank.dao.CustomerDao;
+import bank.dao.AccountDao;
+import bank.model.Account;
+import bank.model.Customer;
+import bank.model.Operation;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class BankingService {
+  public  Account createAccount(Customer customer,String type, BigDecimal initialBalance,BigDecimal interestRate){
+    AccountDao accountDao = new AccountDao();
+    return accountDao.createAccount(customer,type,initialBalance,interestRate);
+  }
 
+  // Customers
+  public Customer createCustomer(String phone, String address, String firstName, String lastName,String email) {
+    CustomerDao customerDao = new CustomerDao();
+    return customerDao.createCustomer(phone,address,firstName,lastName,email);
+  }
+
+  public List<Customer> listCustomers() {
+    CustomerDao customerDao = new CustomerDao();
+    return customerDao.findAll();
+  }
+
+  public List<Account> listAccounts() {
+    AccountDao accountDao = new AccountDao();
+    return accountDao.findAll();
+  }
+
+
+  public Account getAccount(String accountNumber, boolean includeHistory) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  // Operations
+  public void deposit(String accountNumber, BigDecimal amount, Long advisorId) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void withdraw(String accountNumber, BigDecimal amount, Long advisorId) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public void transfer(String fromAccountNumber, String toAccountNumber, BigDecimal amount, Long advisorId) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public List<Operation> getHistory(String accountNumber) {
+    throw new UnsupportedOperationException("Not implemented");
+  }
+
+  public Account getAccountById(int accountId) {
+    AccountDao accountDao = new AccountDao();
+    return accountDao.findById(accountId);
+  }
+
+  public Boolean deposit(Account account, BigDecimal amount) {
+    AccountDao accountDao = new AccountDao();
+    return accountDao.deposit(account,amount);
+  }
+
+  public Boolean transfer(BigDecimal amount,Account sourceAccount,Account targetAccount){
+    AccountDao accountDao = new AccountDao();
+    return accountDao.transfer(amount,sourceAccount,targetAccount);
+  }
+
+  public void createCustomer(String firstName, String lastName) {
+  }
 }
