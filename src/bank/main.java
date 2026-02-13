89cb3a20c03a46504ee99ee8184e58035a8499b5
Rémi Campistron <remi.campistron@gmail.com>
2026-02-13 13:35:50 +0100

    implémentation de la logique du fichier main

--- src/bank/main.java
+++ src/bank/main.java
@@ -1,4 +1,8 @@
-void main() {
+import bank.service.BankingService;
 
+void main() {
+  var service = new BankingService(/* customerDao, accountDao, ... */);
+  var menus = new bank.view.ConsoleUi(service);
+  menus.run();
 
 }
